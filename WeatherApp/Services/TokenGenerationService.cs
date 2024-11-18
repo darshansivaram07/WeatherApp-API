af232f91ca5fb2c943cfe700e45aa4c388782834
Darshan <darshansivaram07@gmail.com>
2024-11-18 21:31:24 +0530

    Added Jwt authentication

---
+++ WeatherApp/Services/TokenGenerationService.cs
@@ -0,0 +1,41 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace WeatherApp.Services
+{
+    public class TokenGenerationService
+    {
+        private readonly IConfiguration _configuration;
+        public TokenGenerationService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string GenerateToken()
+        {
+            var jwtSettings = _configuration.GetSection("JwtSettings");
+            var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]);
+            var issuer = jwtSettings["Issuer"];
+            var audience = jwtSettings["Audience"];
+            var expiryInMinutes = int.Parse(jwtSettings["ExpiryInMinutes"]);
+
+            var claims = new[]
+            {
+            new Claim(JwtRegisteredClaimNames.Sub, "WeatherApiAccess"),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+            var credentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: issuer,
+                audience: audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(expiryInMinutes),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
